da27c0402fe5bbaa765a7079aefae1f17ace59ab
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-10-26 17:42:13 +0100

    prove di funzionamento

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+
+class StudentController extends Controller
+{
+  public function index()
+  {
+    $student = Student::all();
+  }
+
+}
